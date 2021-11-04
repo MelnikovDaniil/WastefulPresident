8d92a69da9c5fa706d08aa2177d787303d812e21
MelnikovDaniil <3148dan@gmail.com>
2021-11-04 21:38:59 +0300

    Repository created

---
+++ Assets/Model/Character/Character.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    [Space]
+    public float checkGroundOffsetY = -1.8f;
+    public float checkFroundRadius = 0.3f;
+
+    private Rigidbody2D _rigidbody;
+    private float horizontalMove = 0;
+
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        if (isGrounded && Input.GetKeyDown(KeyCode.W))
+        {
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+    }
+
+    public void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector2(horizontalMove, _rigidbody.velocity.y);
+        CheckGround();
+    }
+
+    private void CheckGround()
+    {
+        var position = new Vector2(transform.position.x, transform.position.y + checkGroundOffsetY);
+        var colliders = Physics2D.OverlapCircleAll(position, checkFroundRadius);
+
+        isGrounded = colliders.Length > 1;
+    }
+
+    private void OnDrawGizmos()
+    {
+        var position = new Vector2(transform.position.x, transform.position.y + checkGroundOffsetY);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(position, checkFroundRadius);
+    }
+}
