3eb18bc47ec18f6f9cfdc28b15ba771e764decfc
PewBew <55095098+Chepunich@users.noreply.github.com>
2022-04-03 22:32:31 +0300

    Added levels 19-24

--- Assets/Model/Consumer/Trampoline/Trampoline.cs
+++ Assets/Model/Consumer/Trampoline/Trampoline.cs
@@ -17,9 +17,9 @@ public class Trampoline : PowerConsumer
     private Animator _animator;
     private int characterLayer;
     // Start is called before the first frame update
-    public new void Start()
+    public new void Awake()
     {
-        base.Start();
+        //base.Start();
         _animator = GetComponent<Animator>();
         characterLayer = LayerMask.NameToLayer("Characters");
         var boxCollider = gameObject.AddComponent<BoxCollider2D>();
