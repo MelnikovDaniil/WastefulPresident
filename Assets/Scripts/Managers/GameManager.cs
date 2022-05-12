adee8c2980abcbb908b55f55097998ac5a927dd6
daniil.melnikau <daniil_melnikau1@epam.com>
2022-05-13 01:28:11 +0300

    Adjust added

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using com.adjust.sdk;
 
 public class GameManager : BaseManager
 {
@@ -43,6 +44,16 @@ public class GameManager : BaseManager
         MaxSdk.SetUserId(SystemInfo.deviceUniqueIdentifier);
         MaxSdk.SetVerboseLogging(true);
         MaxSdk.InitializeSdk();
+
+        // import this package into the project:
+        // https://github.com/adjust/unity_sdk/releases
+#if UNITY_IOS
+        /* Mandatory - set your iOS app token here */
+        //InitAdjust("YOUR_IOS_APP_TOKEN_HERE");
+#elif UNITY_ANDROID
+        /* Mandatory - set your Android app token here */
+        InitAdjust("dyqevqya0zy8");
+#endif
 
         if (!destroyOnLoad)
         {
@@ -106,5 +117,22 @@ public class GameManager : BaseManager
             var currentLevel = SceneManager.GetActiveScene();
             Instance.StartCoroutine(Instance.LoadLevelRoutine(currentLevel.name, delay));
         }
+    }
+
+    private void InitAdjust(string adjustAppToken)
+    {
+        var adjustConfig = new AdjustConfig(
+            adjustAppToken,
+            AdjustEnvironment.Production, // AdjustEnvironment.Sandbox to test in dashboard
+            true
+        );
+        adjustConfig.setLogLevel(AdjustLogLevel.Info); // AdjustLogLevel.Suppress to disable logs
+        adjustConfig.setSendInBackground(true);
+        new GameObject("Adjust").AddComponent<Adjust>(); // do not remove or rename
+        // Adjust.addSessionCallbackParameter("foo", "bar"); // if requested to set session-level parameters
+        //adjustConfig.setAttributionChangedDelegate((adjustAttribution) => {
+        //  Debug.LogFormat("Adjust Attribution Callback: ", adjustAttribution.trackerName);
+        //});
+        Adjust.start(adjustConfig);
     }
 }
